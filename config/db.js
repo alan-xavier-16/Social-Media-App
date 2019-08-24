df68dc35505565ee373949cc34bd3f15dacad2a4
Alan Xavier <alan_xvr@hotmail.com>
2019-08-24 19:04:28 -0400

    Add logic to User Registration - Check if user exists via email, - Creates a new User if false, - Encrypts password using bcrypt, - Saves user to database. - Remove error in mongoose.connect in db.js

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const db = config.get("mongoURI"); // Gets mongoURI from default.json
 // Connect to MongoDB
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useNewUrlParser: true });
+    await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true });
     console.log("MongoDB Connected...");
   } catch (err) {
     console.error(err.message);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,11 @@
 /*Routes for register, add, etc. for Users*/
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
 
 /* 
 @route    POST api/users 
@@ -20,12 +24,45 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
+    /*  Validates user information using express-validator and returns an array of objects for each error */
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() }); // Returns an array of objects corresponding to each error check
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User route");
+    /* 
+    Check if user exists via email and returns error is true, else
+      1. Gets user's gravatar url from the email,
+      2. Creates a new User,
+      3. Encrypts password using bcrypt module, and 
+      4. Returns a jsonwebtoken (needed for React front-end)
+    */
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+      const avatar = gravatar.url(email, { s: "200", r: "pg", d: "mm" });
+      user = new User({
+        name,
+        email,
+        password,
+        avatar
+      });
+      /* Hashing and Salting Password */
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      /* Save User to Database */
+      await user.save();
+
+      res.send("User registered");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
