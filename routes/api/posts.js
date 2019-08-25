0ade8740e0de31fd8e5cf097b9af326be8fb31e0
Alan Xavier <alan_xvr@hotmail.com>
2019-08-25 18:46:30 -0400

    Add routes to GET all posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -48,4 +48,19 @@ router.post(
   }
 );
 
+/* 
+@route    GET api/posts 
+@desc     Lists all posts
+@access   Private
+*/
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server Error");
+  }
+});
+
 module.exports = router;
