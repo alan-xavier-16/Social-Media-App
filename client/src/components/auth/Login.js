683de41349436c0939173a67922f5e66da04417d
Alan Xavier <alan_xvr@hotmail.com>
2019-08-31 17:59:06 -0400

    Add Login logic to Login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,66 @@
 /*Login Page */
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  /*Set initial state of form with fields blank */
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  /*Handle Form Logic */
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  /*Handle Form Submit */
+  const handleSubmit = async e => {
+    e.preventDefault();
+    console.log("SUCCESS");
+  };
+
+  const { email, password } = formData;
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign Into Your Account
+      </p>
+
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+            minLength="6"
+            required
+          />
+        </div>
+
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </Fragment>
+  );
 };
 
 export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,6 @@
 /*Register a New User */
 import React, { Fragment, useState } from "react";
-import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   /*Set initial state of form with fields blank */
@@ -22,24 +22,7 @@ const Register = () => {
     if (password !== password2) {
       console.log("Passwords do not Match");
     } else {
-      /*Testing out Backend with React */
-      const newUser = {
-        name,
-        email,
-        password
-      };
-      try {
-        const config = {
-          headers: {
-            "Content-Type": "application/json"
-          }
-        };
-        const body = JSON.stringify(newUser);
-        const res = await axios.post("/api/users", body, config);
-        console.log(res.data);
-      } catch (err) {
-        console.error(err.response.data);
-      }
+      console.log("SUCCESS");
     }
   };
 
@@ -107,7 +90,7 @@ const Register = () => {
       </form>
 
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   );
