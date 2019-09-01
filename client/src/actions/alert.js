ea44553d5e1651daa20738940b46b8bdaa3c08fa
Alan Xavier <alan_xvr@hotmail.com>
2019-08-31 21:43:30 -0400

    Add remove alert action to alert.js

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -6,10 +6,22 @@ import { SET_ALERT, REMOVE_ALERT } from "./types";
 import uuid from "uuid";
 
 /* DISPATCHES MULTIPLE ACTION TYPES USING 'DISPATCH' FROM 'THUNK' MIDDLEWARE */
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
   const id = uuid.v4();
+
+  // Set an alert
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id }
   });
+
+  // Removes alert after 5s
+  setTimeout(
+    () =>
+      dispatch({
+        type: REMOVE_ALERT,
+        payload: id
+      }),
+    timeout
+  );
 };
