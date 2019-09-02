e6f45827482f699fc98b944573b6cf49117c55e2
Alan Xavier <alan_xvr@hotmail.com>
2019-09-02 17:13:10 -0400

    Create AddExperience component - Add UPDATE_PROFILE action type in actions/types.js - Create addExperience and addEducation actions to receive data from the Redux store - Add reducer to manage state for UPDATE_PROFILE action - Create AddExperience component - Add private route to AddExperience in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import CreateProfile from "./components/profile-form/CreateProfile";
 import EditProfile from "./components/profile-form/EditProfile";
+import AddExperience from "./components/profile-form/AddExperience";
 
 import "./App.css";
 
@@ -46,6 +47,11 @@ const App = () => {
               component={CreateProfile}
             />
             <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+            <PrivateRoute
+              exact
+              path="/add-experience"
+              component={AddExperience}
+            />
           </Switch>
         </section>
       </Fragment>
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_PROFILE, PROFILE_ERROR } from "./types";
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from "./types";
 
 /* ACTION FOR GET CURRENT USER PROFILE */
 export const getCurrentProfile = () => async dispatch => {
@@ -48,6 +48,78 @@ export const createProfile = (
     if (!edit) {
       history.push("/dashboard");
     }
+  } catch (err) {
+    /* Catches all errors and displays them, such as missing form fields */
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status
+      } /* Gets error message text and status */
+    });
+  }
+};
+
+/* ACTION FOR ADD EXPERIENCE */
+export const addExperience = (formData, history) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    const res = await axios.put("/api/profile/experience", formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert("Experience Added", "success"));
+    /* Redirect to Dashboard */
+    history.push("/dashboard");
+  } catch (err) {
+    /* Catches all errors and displays them, such as missing form fields */
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status
+      } /* Gets error message text and status */
+    });
+  }
+};
+
+/* ACTION FOR ADD EDUCATION */
+export const addEducation = (formData, history) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    const res = await axios.put("/api/profile/education", formData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert("Education Added", "success"));
+    /* Redirect to Dashboard */
+    history.push("/dashboard");
   } catch (err) {
     /* Catches all errors and displays them, such as missing form fields */
     const errors = err.response.data.errors;
